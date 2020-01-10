dabf9ad531ae3ab61b2c5c69edc8210364e2947f
Matt Larsen <larsen30@llnl.gov>
2020-01-10 09:49:38 -0800

    lets try icons

--- gen_readme.py
+++ gen_readme.py
@@ -1,27 +1,27 @@
 import os
 
-def red(text):
-  res = '<span style=\"color:red\">' + text +'</span>'
+def red():
+  res = '![red](./images/red.png)'
   return res
 
-def green(text):
-  res = '<span style=\"color:green\">' + text +'</span>'
+def green():
+  res = '![green](./images/green.png)'
   return res
 
 build_dir = './builds'
 builds = [dI for dI in os.listdir(build_dir) if os.path.isdir(os.path.join(build_dir,dI))]
 builds.sort(reverse=True)
-print builds
+print (builds)
 
 max_size = 10
 
 num_builds = min(len(builds), max_size)
-print num_builds
+print (num_builds)
 
 build_results = []
 
 for i in range(0, num_builds):
-  print builds[i]
+  print (builds[i])
   report_file = build_dir + '/' + builds[i] + '/report.txt'
   report = open(report_file, 'r')
   build_results.append(report.readline().strip('\n'))
@@ -35,11 +35,11 @@ readme.write("# Lassen Builds Results\n\n")
 for i in range(0, num_builds):
   text = ""
   if build_results[i] == 'success':
-    text = green('success')
+    text = green() + ' success'
   elif build_results[i] == 'failure':
-    text = red('failed')
+    text = red() + ' failure'
   else:
-    print "no match"
+    print("no match")
 
   result = ' - ' + builds[i] + ' : ' + text + '\n'
   readme.write(result)
