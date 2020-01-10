339881cf1489806ce24f69e52d4ebc9d6ca50b27
mclarsen <larsen30@llnl.gov>
2020-01-10 09:34:34 -0800

    testing readme gen

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lassen Builds Results
+
+ - 2020-01-10-07:56:12 : <span style="color:green">success/span>
+ - 2020-01-10-07:56:00 : <span style="color:red">failed/span>
---
+++ gen_readme.py
@@ -0,0 +1,48 @@
+import os
+
+def red(text):
+  res = '<span style=\"color:red\">' + text +'/span>'
+  return res
+
+def green(text):
+  res = '<span style=\"color:green\">' + text +'/span>'
+  return res
+
+build_dir = './builds'
+builds = [dI for dI in os.listdir(build_dir) if os.path.isdir(os.path.join(build_dir,dI))]
+builds.sort(reverse=True)
+print builds
+
+max_size = 10
+
+num_builds = min(len(builds), max_size)
+print num_builds
+
+build_results = []
+
+for i in range(0, num_builds):
+  print builds[i]
+  report_file = build_dir + '/' + builds[i] + '/report.txt'
+  report = open(report_file, 'r')
+  build_results.append(report.readline().strip('\n'))
+  report.close()
+
+print(build_results)
+
+readme = open("README.md","w")
+
+readme.write("# Lassen Builds Results\n\n")
+for i in range(0, num_builds):
+  text = ""
+  if build_results[i] == 'success':
+    text = green('success')
+  elif build_results[i] == 'failure':
+    text = red('failed')
+  else:
+    print "no match"
+
+  result = ' - ' + builds[i] + ' : ' + text + '\n'
+  readme.write(result)
+
+readme.close()
+
